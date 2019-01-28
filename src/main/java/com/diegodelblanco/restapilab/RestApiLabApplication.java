eb8d5f90fb8e2566f4904ee282855c7803a6c3be
ddelblanco <diegodelblanco.ingeniero@gmail.com>
2019-01-28 18:36:55 -0500

    Spring Security

--- src/main/java/com/diegodelblanco/restapilab/RestApiLabApplication.java
+++ src/main/java/com/diegodelblanco/restapilab/RestApiLabApplication.java
@@ -1,8 +1,15 @@
 package com.diegodelblanco.restapilab;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class RestApiLabApplication {
 
